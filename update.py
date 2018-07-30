8a327ec0f64d7537fd6677bc3e9fa85d9538b94e
Mofugami <ray.sakanoue@gmail.com>
2018-07-29 23:25:12 -0700

    add updater.py

---
+++ update.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+import os, shutil
+
+driver_path = r""
+folder_path = ""
+download_path = ""
+
+def update_folder(target_folder, filename):
+    if not os.path.exists(folder_path):
+        os.mkdir(folder_path)
+    move_file = download_path + "\\" + filename
+    while not os.path.exists(move_file):
+        pass
+    shutil.move(move_file, target_folder + "\\" + filename)
+
+def task():
+    browser = webdriver.Chrome(exectuable_path=driver_path)
+    browser.get("https://books.goalkicker.com/")
+    books_url = []
+    book_container = browser.find_elements_by_class_name("bookContainer")
+    for element in book_container:
+        link = element.find_element_by_css_selector("a").get_attribute("href")
+        books_url.append(link)
+    for url in books_url:
+        browser.get(url)
+        browser.find_element_by_class_name("download").click()
+        innertext = browser.execute_script("return arguments[0].textContent", browser.find_element_by_id("footer"))
+        split_text = innertext.split(" ")
+        file_name = split_text[0]
+        update_folder(folder_path, file_name)
+    browser.close()
+
+task()
+        
